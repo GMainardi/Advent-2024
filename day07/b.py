7d8c8c2ecd8ac55e6f55122153a2f0ea62a34c2e
GMainardi <guidoluis.gm@gmail.com>
2024-12-07 09:38:58 -0300

    day 07 ⭐⭐

---
+++ day07/b.py
@@ -0,0 +1,19 @@
+
+def check_possible_number(goal, value, numbers):
+    if len(numbers) == 0:
+        return value == goal
+    
+    next = numbers.pop(0)
+    return any([check_possible_number(goal, value + next, numbers.copy()),
+                check_possible_number(goal, value * next, numbers.copy()),
+                check_possible_number(goal, int(str(value) + str(next)), numbers.copy()),])
+
+correct_lines = 0
+for line in open('day07/input.txt', 'r').readlines():
+    line = line.strip()
+    goal = int(line.split(':')[0])
+    numbes = list(map(int, line.split(':')[1].split(' ')[1:]))
+    
+    if check_possible_number(goal, 0, numbes.copy()):
+        correct_lines += goal
+print(correct_lines)
