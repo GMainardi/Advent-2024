6fadd3f68d2c528cb45d35f1227bbf0b6e36d9d0
GMainardi <guidoluis.gm@gmail.com>
2024-12-10 09:26:42 -0300

    day 10 ⭐⭐

---
+++ day10/b.py
@@ -0,0 +1,35 @@
+UP = -1+0j
+DOWN = 1+0j
+LEFT = 0-1j
+RIGHT = 0+1j
+
+def valid_move(curr, hiking_map):
+    return 0 <= curr.real < len(hiking_map) and 0 <= curr.imag < len(hiking_map[0])
+
+def get_map_pos(curr, hiking_map):
+    return hiking_map[int(curr.real)][int(curr.imag)]
+
+def count_trailhead_score(curr, hiking_map):
+
+    if get_map_pos(curr, hiking_map) == 9:
+        return 1
+    
+    score = 0
+    for direction in [UP, DOWN, LEFT, RIGHT]:
+        next = curr + direction
+        if valid_move(next, hiking_map) \
+        and get_map_pos(next, hiking_map) - get_map_pos(curr, hiking_map) == 1:
+            score += count_trailhead_score(next, hiking_map)
+    
+    return score
+
+hiking_map = [list(map(int, line.strip())) for line in open('day10/input.txt', 'r').readlines()]
+
+
+map_score = 0
+for idx, line in enumerate(hiking_map):
+    for jdx, char in enumerate(line):
+        if char == 0:
+            map_score += count_trailhead_score(complex(idx, jdx), hiking_map)
+
+print(map_score)
